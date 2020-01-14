7f988d9b0bcc33371c4c68f26e60767bef118470
sampaiofsky <sampaiofsky97@gmail.com>
2020-01-14 22:13:56 -0100

    j

--- avaliar/avaliar.php
+++ avaliar/avaliar.php
@@ -1,10 +1,9 @@
 <?php
 session_start();
 $name = $_SESSION['nome'];
-
+$i = 1;
 $File=fopen("../bd/avaliacoes.txt","a");
 
-
-fwrite($File, $name . "," . $_POST['Amb'] . "," . $_POST['Fam'] . "," . $_POST['Lim'] . "\n");
+fwrite($File, $i . "," . $name . "," . $_POST['Amb'] . "," . $_POST['Fam'] . "," . $_POST['Lim'] . "\n");
 
 fclose($File);
