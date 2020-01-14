6ca1f0be787e866188366bbdf1dadd2d89c5e801
sampaiofsky <sampaiofsky97@gmail.com>
2020-01-14 17:39:24 -0100

    ye

--- avaliar/medias.php
+++ avaliar/medias.php
@@ -1,14 +1,8 @@
 <?php
+
 $File=fopen("../bd/avaliacoes.txt","r");
-$sum = array();
 
-//$Line= fgetcsv($File,4096)
-
-//$sum = explode(',',$Line);
-//array_push($s,$sum[3]);
-//$cenas = array_sum($s);
-//echo "$cenas";
-$data = fgetcsv($File, 1000, ",");
-  $data = array();
-
-  echo array_sum($data);
+$Line= fgetcsv($File,4096) ;
+$sum = [$Line[1],$Line[2],$Line[3]] ;
+$avaliacao = round((array_sum($sum)/count($sum)), 1);
+echo $avaliacao;
